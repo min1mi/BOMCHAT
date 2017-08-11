833cf869630b016f1acb442f9b4c5e9cc4e8236f
min1mi <min2mi00@gmail.com>
2017-08-11 16:59:40 +0900

    .

--- chatting/public/js/chat.js
+++ chatting/public/js/chat.js
@@ -1,15 +1,25 @@
-var message = $('#message'),
-    sendBtn = $('#sendBtn'),
-    messageBox = $('#messageBox'),
-    sender = $('.chat-header').attr('value')
+var params = decodeURIComponent(location.href).split('?')[1],
+  myNo = parseInt(params.split('&')[0].split('=')[1]),
+  yourNo = parseInt(params.split('&')[1].split('=')[1]),
+  yourName = params.split('&')[2].split('=')[1],
+  message = $('#message'),
+  sendBtn = $('#sendBtn'),
+  messageBox = $('#messageBox')
 
+console.log(myNo, yourNo, yourName);
 var ws = new WebSocket('ws://192.168.0.19:8888/chat/chat.json');
 // var ws = new WebSocket('ws://192.168.0.77:8888/chat/chat.json');
-ws.onopen = function (event) {
-  console.log(event.data)
+
+
+var url1 = decodeURIComponent(location.href);
+var url2 = location.href.split('?')[0];
+
+history.replaceState(null, null, url2);
+
+ws.onopen = function(event) {
 }
 
-ws.onmessage = function (event) {
+ws.onmessage = function(event) {
   console.log(event.data);
   appendMsg(event.data)
   // messageBox.scrollTop(messageBox.prop('scrollHeight'));
@@ -25,7 +35,7 @@ sendBtn.click(() => {
   var value = JSON.stringify(msg);
 
 
-  ws.send(value/*.replace('\n', '').replace('\r', '') + '\n'*/)
+  ws.send(value /*.replace('\n', '').replace('\r', '') + '\n'*/ )
   message.val('')
 })
 
@@ -39,14 +49,14 @@ message.keyup((e) => {
 
 function appendMsg(event) {
   var text = event.replace(/\r?\n/g, '');
-		text = text.replace(/\s/g, "");
-		if(!text) return;
+  text = text.replace(/\s/g, "");
+  if (!text) return;
 
   value = event.replace(/\r?\n/g, '<br />');
   $('<div>').addClass('cd-content clearfix')
-            .appendTo(messageBox)
-            .append($('<span>').addClass(/*isMyAlias ? "me" : */"you")
-            .html(event))
+    .appendTo(messageBox)
+    .append($('<span>').addClass( /*isMyAlias ? "me" : */ "you")
+      .html(event))
   message.val('')
   message.focus()
   resizeMessageBoxPadding()
@@ -55,24 +65,24 @@ function appendMsg(event) {
 }
 
 function resizeMessageBoxPadding() {
-		var padding = parseFloat(messageBox.css('padding-top'))
+  var padding = parseFloat(messageBox.css('padding-top'))
 
-		if(padding < 10 || padding == 10) {
-			return;
-		}
+  if (padding < 10 || padding == 10) {
+    return;
+  }
 
-		var lastBalloon = $('.cd-content:last')
-		var totalBalloonHeight = parseFloat(lastBalloon.css('height'))
-								+ parseFloat(lastBalloon.css('margin-top'))
-								+ parseFloat(lastBalloon.css('margin-bottom'))
-								+ parseFloat(lastBalloon.css('padding-top'))
-								+ parseFloat(lastBalloon.css('padding-bottom'));
+  var lastBalloon = $('.cd-content:last')
+  var totalBalloonHeight = parseFloat(lastBalloon.css('height')) +
+    parseFloat(lastBalloon.css('margin-top')) +
+    parseFloat(lastBalloon.css('margin-bottom')) +
+    parseFloat(lastBalloon.css('padding-top')) +
+    parseFloat(lastBalloon.css('padding-bottom'));
 
 
-		var result = padding - totalBalloonHeight;
-		if(result < 10) {
-			result = 10;
-		}
+  var result = padding - totalBalloonHeight;
+  if (result < 10) {
+    result = 10;
+  }
 
-		messageBox.css('padding-top', result + 'px')
-	}//resizeMessageBoxPadding()
+  messageBox.css('padding-top', result + 'px')
+} //resizeMessageBoxPadding()
