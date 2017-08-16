7e9ea4c9564ef5ba6b509b640f5d2357fb91bbc8
min1mi <min2mi00@gmail.com>
2017-08-16 16:45:58 +0900

    .

--- chatting/control/chatting-control.js
+++ chatting/control/chatting-control.js
@@ -2,13 +2,13 @@ var moment = require('moment');
 moment().format();
 
 const express = require('express')
-// const datasource = require('../util/datasource')
-// const chatDao = require('../dao/chatDao')
-// const chatService = require('../service/chat-service')
-//
-// const connection = datasource.getConnection()
-// chatDao.setConnection(connection)
-// chatService.setChatDao(chatDao)
+const datasource = require('../util/datasource')
+const chatDao = require('../dao/chatDao')
+const chatService = require('../service/chatService')
+
+const connection = datasource.getConnection()
+chatDao.setConnection(connection)
+chatService.setChatDao(chatDao)
 
 var clients = [];
 
@@ -47,73 +47,74 @@ router.ws('/chat.json', function(ws, req) {
       setCommunicator(myMap)
       return;
     }
+    var nowUTC = moment().utc(),
+        now = moment().format("YYYY-MM-DD HH:mm:ss")
 
     if (myMap.has('oppMap')) {
       var data = JSON.stringify({
         'message': msg,
-        'sender': 'you'
+        'sender': 'you',
+        'readChange': false,
+        'datetime': nowUTC
+
       })
       broadcast(myMap, data)
     }
 
-    // if (myMap.get('isTrainer')) addTrainerChat(myMap, msg)
-    // else addChat(myMap, msg)
+    if (myMap.get('isTrainer')) addTrainerChat(myMap, msg, myMap.has('oppMap'), now)
+    else addChat(myMap, msg, myMap.has('oppMap'), now)
   })
 
-  ws.on('close', function(user) {
-    client.splice(client.indexof(client), 1)
-    console.log('client disconnected')
-  })
-
-  ws.on('error', function(user) {
-    user.splice(user.indexof(client), 1)
-    console.log('error')
-  })
+  ws.on('close', function(reasonCode, description) {
+    var result = removeClient(myMap)
+    console.log(result)
+  }) // ws,on('close')
 });
 
-// function addChat(myMap, msg) {
-//   var now = moment().format("YYYY-MM-DD HH:mm:ss")
-//   chatService.insert({
-//     'tno': myMap.get('opponent'),
-//     'mno': myMap.get('user'),
-//     'msg': msg,
-//     'date': now
-//
-//   }, function(result) {
-//     var data = {
-//       'message': msg,
-//       'sender': 'me'
-//     }
-//     myMap.get('ws').send(JSON.stringify(data));
-//   }, function(error) {
-//     console.log(error)
-//   }) //chatService.insert()
-// } //addChat()
-//
-// function addTrainerChat(myMap, msg) {
-//   var now = moment().format("YYYY-MM-DD HH:mm:ss")
-//   chatService.insert({
-//     'mno': myMap.get('opponent'),
-//     'tno': myMap.get('user'),
-//     'msg': msg,
-//     'date': now
-//
-//   }, function(result) {
-//     var data = {
-//       'message': msg,
-//       'sender': 'me'
-//     }
-//     myMap.get('ws').send(JSON.stringify(data));
-//   }, function(error) {
-//     console.log(error)
-//   }) //chatService.insert()
-// } //addChat()
+function addChat(myMap, msg, confirm, now) {
+  chatService.insert({
+    'tno': myMap.get('opponent'),
+    'mno': myMap.get('user'),
+    'msg': msg,
+    'date': now,
+    'who': myMap.get('user'),
+    'confirm': confirm ? true : false
+
+  }, function(result) {
+    console.log('DB insert success')
+
+  }, function(error) {
+    console.log('DB insert fail')
+    console.log(error)
+
+  }) //chatService.insert()
+} //addChat()
+
+function addTrainerChat(myMap, msg, confirm, now) {
+  chatService.insert({
+    'mno': myMap.get('opponent'),
+    'tno': myMap.get('user'),
+    'msg': msg,
+    'date': now,
+    'who': myMap.get('user'),
+    'confirm': confirm ? true : false
+
+  }, function(result) {
+    console.log('DB insert success')
+
+  }, function(error) {
+    console.log('DB insert fail')
+    console.log(error)
+
+  }) //chatService.insert()
+} //addTrainerChat()
 
 function setCommunicator(myMap) {
   var oppMap;
   for (var i = 0; i < clients.length; i++) {
     oppMap = clients[i]
-    if ((oppMap.get('opponent') == myMap.get('user')) && (oppMap.get('user') == myMap.get('opponent'))) {
+    if ((oppMap.get('opponent') == myMap.get('user')) &&
+        (oppMap.get('user') == myMap.get('opponent'))) {
       console.log('상대도 온라인 상태');
       myMap.set('oppMap', oppMap)
       oppMap.set('oppMap', myMap)
@@ -123,9 +124,69 @@ function setCommunicator(myMap) {
   } //for()
 } //broadcast()
 
+
+function updateRead(myMap) {
+  var data = {
+    'sender': 'you',
+    'readChange': true
+  }
+
+  if (myMap.get(isTrainer)) {
+    chatService.update(myMap.get('opponent'), myMap.get('user'), function(result) {
+      console.log('read Update')
+      broadcast(myMap, JSON.stringify(data)) // 상대에게 내가 들어왔음을 알림
+      myMap.get('ws').send(JSON.stringify(data)) // 나에게 상대도 온라인 상태임을 알림
+    }, function(error) {
+      res.status(200)
+        .set('Content-Type', 'text/plain; charset=UTF-8')
+        .end('error')
+      console.log(error)
+    })
+  } else {
+    chatService.update(myMap.get('user'), myMap.get('opponent'), function(result) {
+      console.log('read Update')
+      broadcast(myMap, JSON.stringify(data)) // 상대에게 내가 들어왔음을 알림
+      myMap.get('ws').send(JSON.stringify(data)) // 나에게 상대도 온라인 상태임을 알림
+    }, function(error) {
+      res.status(200)
+        .set('Content-Type', 'text/plain; charset=UTF-8')
+        .end('error')
+      console.log(error)
+    })
+  }
+}
+
+
 function broadcast(myMap, data) {
-  console.log('브로드 캐스트 => ' + myMap.get('opponent') + ', wsID: ' + myMap.get('oppMap').get('ws')._socket._handle.fd);
-  myMap.get('oppMap').get('ws').send(data)
+  console.log('브로드 캐스트 => ' + myMap.get('opponent'));
+  var userMap = myMap.get('oppMap')
+  console.log(userMap)
+  userMap.get('ws').send(data)
+}
+
+function removeClient(myMap) {
+  var result;
+  var no = myMap.get('user')
+
+  for (var i = 0; i < clients.length; i++) {
+    if(clients[i].get('user') == no) {
+      clients.splice(i, 1) //서버의 클라이언트 목록에서 내 map을 삭제
+      result = no + " user removed from client list "
+      break;
+    }
+  }
+
+  for (var j = 0; j < clients.length; j++) {
+    if(clients[j].get('opponent') == no) {
+      var data = {"exit": "exit"}
+      clients[j].get('ws').send(JSON.stringify(data))//상대에게 내가 나가는 것을 알려주어, 상대가 읽음상태를 가늠할 수 있또록 함
+      clients[j].delete('oppMap')//상대의 map에서 내 map을 제거하여 상대와의 연결을 끊음
+      result += "and from opponent's map"
+      break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = router
--- chatting/public/js/chat.js
+++ chatting/public/js/chat.js
@@ -12,20 +12,16 @@ var params = decodeURIComponent(location.href).split('?')[1],
   timeBefore,
   dateBefore,
   isMyAliasBefore,
-  host = '172.20.10.3';
+  // host = '172.20.10.3';
+  host = '192.168.0.5';
 
 console.log(myNo, yourNo, yourName, memberType, imgPath);
 // var ws = new WebSocket('ws://172.20.10.5:8888/chat/chat.json');
 // var ws = new WebSocket('ws://192.168.0.19:8888/chat/chat.json');
 // var ws = new WebSocket('ws://192.168.0.77:8888/chat/chat.json');
 
-function hideAddressBar(){
-	if(document.documentElement.scrollHeight<window.outerHeight/window.devicePixelRatio)
-		document.documentElement.style.height=(window.outerHeight/window.devicePixelRatio)+'px';
-	setTimeout(window.scrollTo(1,1),0);
-}
-window.addEventListener("load",function(){hideAddressBar();});
-window.addEventListener("orientationchange",hideAddressBar());
+// window.addEventListener("load",function(){hideAddressBar();});
+// window.addEventListener("orientationchange",hideAddressBar());
 
 
 var url1 = decodeURIComponent(location.href);
@@ -59,18 +55,16 @@ function appendMsg(event, isMyAlias, isSendData) {
     message.focus()
   }
   sizeBack()
-  // resizeMessageBoxPadding()
   var viewHeight
     if (messageBox.height() < messageBox.prop("scrollHeight"))
     viewHeight = messageBox.prop("scrollHeight")
     else viewHeight = messageBox.height()
-    // console.log(messageBox.height())
-    // console.log(messageBox.prop("scrollHeight"))
     messageBox.scrollTop(viewHeight)//가장 마지막 메시지가 보일 수 있도록 스크롤을 가장 아래로 내림
 }
 
 function readyChat() {
-  ws = new WebSocket('ws://172.20.10.5:8888/chat/chat.json');
+  // ws = new WebSocket('ws://172.20.10.5:8888/chat/chat.json');
+  ws = new WebSocket('ws://192.168.0.5:8888/chat/chat.json');
 
   console.log('readyChat 실행됨');
   ws.onopen = function(event) {
@@ -104,38 +98,10 @@ sendBtn.on('click', function() {
 
 message.keyup(function(e) {
   if (e.keyCode == 13) {
-    appendMsg(message.val(), true, true)
-    message.val('')
     sendBtn.click() // send 버튼에 click이벤트 발생시킴, 호출X
   }
   // e.preventDefault()
 })
-
-function resizeMessageBoxPadding() {
-  var padding = parseFloat(messageBox.css('padding-top'))
-
-  if (padding < 10 || padding == 10) {
-    return;
-  }
-
-  var lastBalloon = $('.cd-content:last')
-  console.log(lastBalloon)
-  var totalBalloonHeight = parseFloat(lastBalloon.css('height')) +
-    parseFloat(lastBalloon.css('margin-top')) +
-    parseFloat(lastBalloon.css('margin-bottom')) +
-    parseFloat(lastBalloon.css('padding-top')) +
-    parseFloat(lastBalloon.css('padding-bottom'));
-
-
-  var result = padding - totalBalloonHeight;
-  console.log(result)
-  if (result < 10) {
-    result = 10;
-  }
-
-  messageBox.css('padding-top', result + 'px')
-} //resizeMessageBoxPadding()
-
 
 function sizeBack() {
   message.css('height', '6vh')
