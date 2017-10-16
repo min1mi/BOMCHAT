dddb9b1a0895d39d6780cbf05ce97fdef54560e7
kwonbongsoo <star1231076@gmail.com>
2017-10-16 18:43:31 +0900

    cross domain 보인 이슈..
    보안 이슈 설정 변경 코드.

--- chatting/control/alert-control.js
+++ chatting/control/alert-control.js
@@ -13,7 +13,7 @@ const router = express.Router()
 router.post('/get.json', (request, response) => {
   response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
   //- POST, GET, OPTIONS, DELETE 요청에 대해 허용하겠다는 의미입니다.
-  
+
   response.setHeader("Access-Control-Max-Age", "3600");
   // - HTTP Request 요청에 앞서 Preflight Request 라는 요청이 발생되는데,
   //   이는 해당 서버에 요청하는 메서드가 실행 가능한지(권한이 있는지) 확인을 위한 요청입니다.
@@ -29,7 +29,7 @@ router.post('/get.json', (request, response) => {
   // Form을 통한 요청과 Ajax 요청을 구분하기 위해 사용된 비표준 규약지만, 많은 라이브러리에서 이를 채택하여 사용하고 있습니다.
   // (참고로 HTML5 부터는 Form 과 Ajax 요청을 구분할 수 있는 Header가 추가되었습니다.)
 
-  response.setHeader("Access-Control-Allow-Origin", "https://www.bombees.com/");
+  response.setHeader("Access-Control-Allow-Origin", "https://www.bombees.com");
 
   // 이 부분이 가장 중요한 부분입니다.
   // * 는 모든 도메인에 대해 허용하겠다는 의미입니다.
