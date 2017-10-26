221864d5790be3f92c7a74577ba3025f9003edde
kwonbongsoo <star1231076@gmail.com>
2017-10-26 14:13:53 +0900

    1
    1

--- chatting/public/js/chat.js
+++ chatting/public/js/chat.js
@@ -13,7 +13,7 @@ var params = decodeURIComponent(location.href).split('?')[1],
   dateBefore,
   isMyAliasBefore,
   // host = '172.20.10.3';
-  host = location.host;
+  host = location.host; 
 
 console.log(imgPath)
 // var ws = new WebSocket('ws://172.20.10.5:8888/chat/chat.json');
